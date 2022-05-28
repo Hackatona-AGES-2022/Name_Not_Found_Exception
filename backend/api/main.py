676a08a7b7c27ef8b1b0add664e8e73f28bfc5c5
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-05-28 07:36:41 -0300

    More routes and crud operations.

--- backend/api/main.py
+++ backend/api/main.py
@@ -16,6 +16,8 @@ origins = [
 app = FastAPI()
 
 # Dependency
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -23,60 +25,58 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-async def read_root():
-    item = {"Hello": "World"}
-    return item
+
+# POSTS
+
 
 @app.post("/user")
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, user_email=user.email)
+    db_user = crud.get_user_by_email(db=db, email=user.email)
     if(db_user):
-        raise HTTPException(status_code=400, detail="user já existe")
-    return crud.create_user(db=db, user=db_user)
+        raise HTTPException(status_code=400, detail="User already exists.")
+    return crud.create_user(db=db, user=user)
+
 
 @app.post("/store")
 def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db)):
-    #db_store = crud.get_store_by_cnpj(db, store_cnpj=store.cnpj)
-    #if(db_store):
-    #    raise HTTPException(status_code=400, detail="store já existe")
+    db_store = crud.get_store_by_cnpj(db, cnpj=store.cnpj)
+    if(db_store):
+        raise HTTPException(status_code=400, detail="Store already exists.")
     return crud.create_store(db=db, store=store)
+
 
 @app.post("/sale")
 def create_sale(sale: schemas.SaleCreate, db: Session = Depends(get_db)):
-    db_store = crud.get_store_by_id(db, sale_id=sale.id)
-    if(db_store):
-        raise HTTPException(status_code=400, detail="sale já existe")
-    return crud.create_sale(db=db, sale=db_store)
+    return crud.create_sale(db=db, sale=sale)
+
 
 @app.post("/users_sales")
-def create_users_sales(users_sales: schemas.UserSaleCreate, db: Session = Depends(get_db)):
-    db_users_sales = crud.get_users_sales_by_id(db, users_sales_id=users_sales.id)
-    if(db_users_sales):
-        raise HTTPException(status_code=400, detail="users_sales já existe")
-    return crud.create_users_sales(db=db, users_sales=db_users_sales)
-
-@app.get("/users", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+def create_user_sale(user_sale: schemas.UserSaleCreate, db: Session = Depends(get_db)):
+    return crud.create_user_sale(db=db, user_sale=user_sale)
 
 
-@app.get("/sales", response_model=List[schemas.Sale])
-def read_sales(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    sales = crud.get_sales(db, skip=skip, limit=limit)
+# GETS
+
+
+@app.get("/sales")
+def get_all_sales(db: Session = Depends(get_db)):
+    sales = crud.get_all_sales(db=db)
     return sales
 
+
 @app.get("/stores")
-def read_stores(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    stores = crud.get_all_stores(db, skip=skip, limit=limit)
+def get_all_stores(db: Session = Depends(get_db)):
+    stores = crud.get_all_stores(db=db)
     return stores
 
 
-@app.get("/user_sale", response_model=List[schemas.UserSale])
-def read_users_sales(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users_sales = crud.get_users_sales(db, skip=skip, limit=limit)
+@app.get("/user_sales")
+def get_user_sales(user_search: schemas.UserSearchByEmail, db: Session = Depends(get_db)):
+    users_sales = crud.get_user_sales(db=db, email=user_search.email)
     return users_sales
 
 
-#########################################################
+@app.get("/store_sales")
+def get_user_sales(store_search: schemas.StoreSearchByCnpj, db: Session = Depends(get_db)):
+    store_sales = crud.get_user_sales(db=db, cnpj=store_search.cnpj)
+    return store_sales
