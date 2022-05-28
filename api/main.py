df774bcab3fcf8f6963842c489e04a547c9f8892
Alessandro Borges de Souza <alessandro.souza@edu.pucrs.br>
2022-05-28 06:07:44 -0300

    remove pyc files

--- api/main.py
+++ api/main.py
@@ -81,3 +81,40 @@ def read_users_sales(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 
 #########################################################
 
+@app.post("/turmas", response_model=schemas.Turma)
+def create_turmas(turma: schemas.TurmaCreate, db: Session = Depends(get_db)):
+    db_turma = crud.get_turma_by_num_turma(db, num_turma=turma.num_turma)
+    if db_turma:
+        raise HTTPException(status_code=400, detail="Turma já existe")
+    return crud.create_turma(db=db, turma=turma)
+
+@app.get("/turmas", response_model=List[schemas.Turma])
+def read_turmas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    turmas = crud.get_turmas(db, skip=skip, limit=limit)
+    return turmas
+
+
+@app.post("/sale/{preco}", response_model=schemas.Aluno)
+def create_alunos(aluno: schemas.AlunoCreate, db: Session = Depends(get_db)):
+    db_aluno = crud.get_aluno_by_matricula(db, matricula=aluno.matricula)
+    if db_aluno:
+        raise HTTPException(status_code=400, detail="Aluno já existe")
+    return crud.create_aluno(db=db, aluno=aluno)
+
+@app.get("/alunos", response_model=List[schemas.Aluno])
+def read_turmas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    alunos = crud.get_alunos(db, skip=skip, limit=limit)
+    return alunos
+
+
+@app.post("/horarios", response_model=schemas.Horario)
+def create_horarios(horario: schemas.HorarioCreate, db: Session = Depends(get_db)):
+    db_horario = crud.get_horario_by_dia_semana(db, dia_semana=horario.dia_semana)
+    if db_horario:
+        raise HTTPException(status_code=400, detail="Horário já existe")
+    return crud.create_horario(db=db, horario=horario)
+
+@app.get("/horarios", response_model=List[schemas.Horario])
+def read_horarios(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    horarios = crud.get_horarios(db, skip=skip, limit=limit)
+    return horarios
