9632feb2da1c107ad0cc28f511e7a9d71ad8e587
bvinicius <vinicius.bazanella1@gmail.com>
2022-05-28 10:15:54 -0300

    Implementa front da listagem de sales do mercado

--- frontend/src/components/SaleCard.vue
+++ frontend/src/components/SaleCard.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="card__container" v-ripple>
     <div class="card__content">
-      <div class="image">
-        <img src="../assets/arroz.svg" alt="arroz" />
+      <div class="card__title align-self-center">Sal refinado 1KG</div>
+      <div class="card__quantity align-self-center">
+        <div class="card__quantity--low">147</div>
+        <div class="card__quantity--high">/200 un.</div>
       </div>
-      <div class="waves">
-        <div class="sale__title">Sal refinado 1KG</div>
-        <div class="sale_quantity">
-          <span class="sale_quantity--low">147</span>
-          <span class="sale_quantity--high">/200 un.</span>
-        </div>
-      </div>
+      <div class="card__price align-self-center">R$1,74/pct</div>
     </div>
   </div>
 </template>
@@ -29,43 +25,43 @@ export default {
   &__container {
     width: 100%;
     max-width: 165px;
-    height: 200px;
-    background-color: #ececec;
+    height: 180px;
+    background-image: linear-gradient(90deg, #e9fbda, #ffffff);
     border-radius: 16px;
+    border: 2px solid #85db46;
   }
 
   &__content {
     height: 100%;
     display: flex;
     flex-direction: column;
+    padding: 8px;
+    color: #515151;
+    justify-content: space-between;
 
-    .image {
-      height: 50%;
-      align-self: center;
-    }
+    .card {
+      &__title {
+        font-size: 16px;
+        margin-top: 8px;
+        font-weight: 600;
+        text-align: center;
+      }
 
-    .waves {
-      border-radius: 16px;
-      height: 50%;
-      background-image: url(../assets/waves.svg);
-      bottom: 0;
-      padding: 22px 4px 0 4px;
-      color: white;
-      background-position: -120px;
+      &__price {
+        margin-top: 8px;
+        font-size: 20px;
+        font-weight: 600;
+      }
 
-      .sale {
-        &__title {
+      &__quantity {
+        display: flex;
+        &--low {
+          font-size: 20px;
           font-weight: 600;
         }
-
-        &__quantity {
-          display: flex;
-          &--low {
-            font-size: 1.4rem;
-            font-weight: 600;
-          }
-          &--high {
-          }
+        &--high {
+          font-size: 14px;
+          margin-top: 12px;
         }
       }
     }
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-container class="px-8 mt-4">
-    <v-row>
+  <div class="px-8 mt-4 view__container">
+    <v-row class="small mt-4">
       <span class="view__title">Promoções Ativas</span>
     </v-row>
-    <v-row class="mt-6">
+
+    <v-row class="small mt-6">
       <Input
         label="Pesquisar promoções..."
         prepend-inner-icon="mdi-magnify"
@@ -12,27 +13,40 @@
         elevation="3"
       />
     </v-row>
-    <v-row class="justify-space-between mt-8">
+
+    <v-row class="justify-space-between mt-12">
       <v-col cols="6" v-for="sale in sales" :key="sale.id">
         <SaleCard :sale="sale" />
       </v-col>
     </v-row>
-  </v-container>
+
+    <div class="bottom d-flex justify-center">
+      <Button title="Cadastrar Promoção" extended rounded />
+    </div>
+  </div>
 </template>
 
 <script>
 import Input from "../components/Input.vue";
 import { getStoreSales } from "../service/SalesService";
 import SaleCard from "../components/SaleCard.vue";
+import Button from "../components/Button.vue";
 
 export default {
   name: "Home",
-  components: { Input, SaleCard },
+  components: { Input, SaleCard, Button },
   data: () => ({
     sales: [{}],
   }),
   async mounted() {
+    this.$root.showToolbar("Mercado Pão de Açúcar");
     this.sales = await getStoreSales("9696969696");
   },
 };
 </script>
+
+<style scoped>
+.small {
+  max-height: 36px;
+}
+</style>
